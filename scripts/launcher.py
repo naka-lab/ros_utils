aa0e87afaee1a2b5c0a3b6d625e7cc6f365dc454
naka_t <test@test.com>
2021-03-12 10:37:16 +0900

    プログラム起動用のGUIランチャーを追加

---
+++ scripts/launcher.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import print_function, unicode_literals
+import rospy
+import rosparam
+from collections import defaultdict
+import tkinter as tk
+import yaml
+import os
+
+os.chdir( os.path.abspath(os.path.dirname(__file__)))
+
+# パラメータ名保存用dict
+param_dict = defaultdict(list)
+
+# GUIパーツ
+root = tk.Tk()
+root.title("ROSLauncher")
+
+# windowを自動で閉じるかどうか
+is_auto_close = tk.BooleanVar()
+is_auto_close.set(True)
+
+# 実行ボタンを配置するframe
+frame_launcher = tk.Frame(root, relief=tk.GROOVE, borderwidth=2)
+parts = []
+
+
+class Command():
+    def __init__(self, name, com, opt ):
+        self.com = com
+        self.opt = opt
+        self.name = name
+    
+    def __call__(self):
+        print( "call", self.com, self.opt.get() )
+
+        command =  'gnome-terminal --tab --active --title ' + self.name + ' -- bash -c "' + self.com + ' ' + self.opt.get()
+        
+        if not is_auto_close.get():
+            command += '; bash"'
+        else:
+            command += '"'
+
+        print(command)
+        os.system( command )
+
+def selection_changed( pname ):
+    # 設定を読み込む
+    with open('launcher_settings.yaml') as file:
+        commands = yaml.safe_load(file)
+
+    # 表示されているGUIを消去
+    for p in parts:
+        p.pack_forget()
+
+    for command in commands[pname]:
+
+        name = command["name"]
+        com = command["command"]
+        opt = ""
+
+        if "option" in command and command["option"]!=None:
+            opt = command["option"]
+
+        frame = tk.Frame(frame_launcher)
+        frame.pack()
+        parts.append( frame )
+
+        tk.Label(frame, text=name, width=30  ).pack(side="left")
+        opt_entry = tk.Entry(frame, textvariable=tk.StringVar(frame, opt), width=30 )
+        opt_entry.pack(side="left")
+        tk.Button( frame, text='laucnch', command=Command(name, com, opt_entry) ).pack(side="left")
+
+
+
+def main():
+    with open('launcher_settings.yaml') as file:
+        commands = yaml.safe_load(file)
+    packages = commands.keys()
+
+    frame = tk.Frame(root)
+    frame.pack()
+
+    tk.Label(frame, text="package:" ).pack(side="left")
+
+    # パッケージ選択のプルダウンメニュー
+    opt = tk.OptionMenu(frame, tk.StringVar(frame, "general"), *packages, command=selection_changed )
+    opt.config(width=50 )
+    opt.pack(side="left")
+
+    # windowを自動で閉じるかどうかのチェックボックス
+    tk.Checkbutton( frame, variable=is_auto_close ,text="終了したウィンドウを自動で閉じる" ).pack(side="left")
+
+    # 初期選択
+    selection_changed( "general" )
+
+    frame_launcher.pack()
+    root.mainloop()
+        
+if __name__ == "__main__":
+    main()
---
+++ scripts/launcher.sh
@@ -0,0 +1,2 @@
+#!/bin/bash -i
+gnome-terminal -- bash -c "rosrun ros_utils launcher.py; bash"
